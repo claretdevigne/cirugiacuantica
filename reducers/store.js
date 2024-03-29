030fba224be4eeca6053020ead7e65ff0fb4f233
Claret Devigne <57070276+claretdevigne@users.noreply.github.com>
2024-03-28 22:34:36 -0500

    Delete reducers directory

--- reducers/store.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-
-export const useStore = create((set) => ({
-  currentStudents: [],
-  oldStudents: [],
-  setCurrentStudents: (payload) => (state) => set({ currentStudents: payload}) ,
-  setOldStudents: (payload) => (state) => set({ oldStudents: payload}),
-}))
